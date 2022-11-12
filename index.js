d37f430e48db8380cd02bad8fd0160f7cb4cd298
Hassan Razak <hassanrazak1991@gmail.com>
2022-11-12 10:32:30 -0500

    commit tree 3ed0c9c2655a4c97f87d9d824616c1bf1b44bb51

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+
+function clickAlert(){
+    alert('I was clicked');
+};
+
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    return input.addEventListener('click',clickAlert);
+};
+
+addingEventListener();
+
+
